eb4977b2af3e887d98a45962b6f4dc24c7f9d690
Louis Cano <louis.cano@hotmail.fr>
2024-07-04 14:31:43 +0200

    lists ok

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,5 +1,6 @@
 class List < ApplicationRecord
   has_many :bookmarks, dependent: :destroy
   has_many :movies, through: :bookmarks, dependent: :destroy
+  has_and_belongs_to_many :movies
   validates :name, presence: true, uniqueness: true
 end
